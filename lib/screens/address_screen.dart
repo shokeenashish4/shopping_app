03e60cd835953226d216ea75c3517e0b160e4b4c
Ashish Shokeen <shokeenashish4@gmail.com>
2023-12-08 11:58:24 +0530

    Address screen with Name, Phone number and Address fields.

--- lib/screens/address_screen.dart
+++ lib/screens/address_screen.dart
@@ -1,4 +1,5 @@
 import 'package:figma_shopping_app/screens/onboarding_screen_scaffold.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AddressScreen extends StatelessWidget {
@@ -7,10 +8,149 @@ class AddressScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return OnboardingScreenScaffold(
+      appbarTitle: "Address",
       buttonText: "Save Address",
       onButtonClick: () {},
-      child: const Column(
-        children: [],
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 20,
+          right: 20,
+          bottom: 100,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Name",
+              style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const InputField(
+              currHintText: "Naam Likho",
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Country",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        InputField(currHintText: "Country Batao"),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "City",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        InputField(currHintText: "City Batao"),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Text(
+              "Phone Number",
+              style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const InputField(currHintText: "No. Likho"),
+            const Text(
+              "Address",
+              style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const InputField(currHintText: "Poora Address Likho"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Save as Primary Address",
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(
+                  width: 28,
+                  child: Transform.scale(
+                    scale: 0.6,
+                    child: CupertinoSwitch(
+                      value: true,
+                      onChanged: (isChecked) {},
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class InputField extends StatelessWidget {
+  const InputField({
+    super.key,
+    required this.currHintText,
+  });
+
+  final String currHintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10, bottom: 10),
+      child: TextField(
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: const Color(0xFFF5F6FA),
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              width: 1,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              width: 1,
+              color: Color(0xFF8F959E),
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          hintText: currHintText,
+        ),
+        maxLines: null,
       ),
     );
   }
